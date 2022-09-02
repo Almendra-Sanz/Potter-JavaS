211c7112f697e83ec128c6c883a19fd93700a7aa
Almendra <almensanz.as@gmail.com>
2022-09-02 22:21:57 +0200

    Primer desafio entregable JS

--- main.js
+++ main.js
@@ -1,4 +1,28 @@
-let hechizo =prompt("Elige el hechizo que deseas" + "\nAccio" + "\nConfundus" + "\nAlohomora");
+function hello(persona, hogar) {
+  alert ("Bienvenide a ollivander " + persona + " de " + hogar)
+}
+
+function datos() {
+    let nombre = prompt ("¿Cual es tu nombre?")
+    let casa = prompt ("¿A que casa de Hogwarts perteneces?")
+    hello(nombre, casa)
+}
+
+
+function consulta() {
+    let respuesta = prompt("¿Desea elegir otro hechizo?" + "\nSi" + "\nNo")
+    return respuesta
+}
+
+function hech() {
+    let hechizo =prompt("Elige el hechizo que deseas" + "\nAccio" + "\nConfundus" + "\nAlohomora");
+    return hechizo
+
+}
+
+
+datos()
+let hechizo = hech()
 while (hechizo.toLowerCase() == "Accio" || "Confundus" || "Alohomora") {
     if (hechizo == "accio" || hechizo == "Accio") {
     alert("Se utiliza para atraer un objeto sea cual sea la distancia a la que esté"); 
@@ -10,8 +34,8 @@ while (hechizo.toLowerCase() == "Accio" || "Confundus" || "Alohomora") {
     alert("Hechizo no encontrado");
 }
 
-let consulta = prompt("¿Desea elegir otro hechizo?" + "\nSi" + "\nNo")
-    if (consulta.toLowerCase() == "si") {
+let pregunta = consulta()
+    if (pregunta.toLowerCase() == "si") {
         hechizo =prompt("Elige el hechizo que deseas" + "\nAccio" + "\nConfundus" + "\nAlohomora");
     } else {
         alert ("Travesura terminada")

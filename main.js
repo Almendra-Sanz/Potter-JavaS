d60f241ee957c21afa3ae11a4901d6b88b680d0b
Almendra <almensanz.as@gmail.com>
2022-09-28 00:01:53 +0200

    segunda entrega proyecto final

--- main.js
+++ main.js
@@ -1,138 +1,135 @@
 
 
-
-function hello(persona) {
-    alert ("Bienvenide a ollivander " + persona + ", aqui encontraras una enciclopedia de hechizos colaborativa")
-  }
-
-  
-
-  
-  function datos() {
-      let nombre = prompt ("¿Cual es tu nombre?")
-      hello(nombre)
-  }
-  
-  
-  class Hechizo {
-      constructor (id, nombre, funcion) {
-          this.nombre = nombre,
-          this.funcion = funcion
-  
-      }
-   
-      mostrar() {
-        let lista = document.getElementById("lista")
-        let hechizo = document.createElement("li");
-        hechizo.innerHTML =(`El hechizo ${this.nombre} se utiliza para ${this.funcion}`) ; 
-        lista.append(hechizo);
-      }
-  }
-  
-  const hechizo1 = new Hechizo ("1","Accio", "para atraer un objeto sea cual sea la distancia a la que esté")
-  const hechizo2 = new Hechizo ("2","Alohomora","abrir cerraduras cerradas con llave")
-  const hechizo3 = new Hechizo ("3","Confundus","causar confusión en las personas o encantar objetos")
-  const hechizo4 = new Hechizo ("4","Aqua eructo","generar un potente chorro de agua que salga de la punta de la varita, puede ayudar a combatir incendios")
-  const hechizo5 = new Hechizo ("5","Avis", "invocar a una bandada de aves")
-  const hechizo6 = new Hechizo ("6","Anapneo", "despejar las vias respiratorias de las personas")
-  const hechizo7 = new Hechizo ("7","Avada Kedavra", "matar a cualquier ser vivo que este a su alncance.Es una de las maldiciones imperdonables y no posee contra hechizo.")
-  
-  
-  const enciclopedia = [hechizo1, hechizo2, hechizo3, hechizo4, hechizo5, hechizo6, hechizo7]
- 
- 
-
-  function explorar(array) {
-      array.forEach((Hechizo) => {
-          Hechizo.mostrar()
-          
-      })
+class Card {
+  constructor (nombre, fun, img) {
+      this.nombre = nombre,
+      this.fun = fun, 
+      this.img = img
       
-      } 
-  
-  function buscarNombre(array) {
-      let buscar = prompt("Ingrese el nombre del hechizo que desea encontrar")
-      let nombreEncontrado = enciclopedia.find((hechizo)=> hechizo.nombre.toLowerCase() == buscar.toLowerCase())
-      if(nombreEncontrado == undefined){
-          alert("El hechizo no se encuentra disponible por el momento")
-      }else{
-          alert("El hechizo se encuentra en la enciclopedia")
-          alert(`El hechizo ${nombreEncontrado.nombre} se utiliza para ${nombreEncontrado.funcion}`)
-         
-      }
-      susDeseos()
-  }
-  
-  function incluirHechizo(array) {
-      let nombreIn = prompt("Ingrese el nombre del hechizo")
-      let funcionIn = prompt ("Describi la funcion del hechizo ingresado anteriormente ")
-      let hechizoNuevo = new Hechizo (array.length+1, nombreIn, funcionIn)
-      let confirmacion = prompt (`${nombreIn}, ${funcionIn}
-        Confirma que quiere incluir el hechizo a la enciclopedia? Si/No`) 
-         if (confirmacion.toLowerCase() != "si") {
-            susDeseos()
-       
-        } else {
-            array.push(hechizoNuevo)
-            console.log(hechizoNuevo)
-        let otroHechizo = prompt ("Desea ingresar otro hechizo?")
-        if (otroHechizo != "si") {
-            susDeseos()
-        } else {
-            incluirHechizo()
-        }
-            
-
-        }
-   
   }
 
-  const botoncito = document.querySelector("button");
-  botoncito.addEventListener("click", susDeseos)
+}
+
+const card1 = new Card ("THE FOOL","The Fool, the first card in the tarot, is a symbol of naivety and childlike wonder. This card is eager and ready to take a risk and start a new journey, always with a youthful spirit,astrologer and tarot practitioner Imani Quinn tells Bustle. Its also a reminder to proceed with caution", "Fool.jpg")
+const card2 = new Card ("THE MAGICIAN","The Magician reminds you of your ability to design your dream life. “You have all of the tools you need to manifest your desires, be ready to use whichever one is called for at any moment,” says Quinn.","magician.jpg")
+const card3 = new Card ("THE EMPEROR","When the Emperor shows up, it’s highlighting an authoritative power or a tendency to be in control. “There's often a need to parent yourself when this card shows up. It's a powerful sign that asks you to lean into your sense of security, stability, and structure,” explains Marquardt.","emperor.jpg")
+const card4 = new Card ("THE EMPRESS","If the Empress has made an appearance, you’re being asked to step into your creative and sexual power. “It's a perfect card for self-love, but it also might show the need to nurture something very important to you, whether it's a child, a relationship or a creative project,” Ryan Marquardt, an astrologer and tarot practitioner, tells Bustle.", "empress.jpg")
+const card5 = new Card ("THE HIEROPHANT","The Hierophant's stern and austere energy is about carrying legacy and traditions. “You might be asked to stick to the plan and not skip over any steps,” explains Marquardt. “It also has a lot to do with your belief systems, religious influences, and philosophies on life.”","hierophant.jpg")
+const card6 = new Card ("THE LOVERS","Sure, the Lovers can indicate a union or a new boo on the horizon, “but it's really about establishing harmony when faced with a big choice in life,” says Marquardt. Ultimately, this card acknowledges that there’s an important decision that needs to be made.", "lovers.jpg")
+const card7 = new Card ("THE PRIESTESS","The High Priestess invites you to follow your gut and accept what your inner knowing is already telling you. “Trust your intuition and see it as your superpower, allow yourself and others to move from an empowered intuitive place,” Quinn explains","priestess.jpg")
+const card8 = new Card ("THE STRENGHT","While the Strength card symbolizes grit and power, it’s also about generosity. “The Strength card is a very positive one that asks you to stay compassionate with yourself and others, and give where you can, because when you give of yourself you'll ultimately receive more in return,” Marquardt explains.", "strength.jpg")
+
+let enciclopedia =[]
+
+
+// Guardar enciclopedia en estorage
+if(localStorage.getItem("enciclopedia")) {
   
-  
-  function susDeseos() {
+  enciclopedia = JSON.parse(localStorage.getItem("enciclopedia"))
+}
+else{
+  enciclopedia.push(card1, card2, card3, card4, card5, card6, card7, card8)
+localStorage.setItem("enciclopedia", JSON.stringify(enciclopedia))
+}
+// Llamar DOM
+
+const inputNombre = document.getElementById ("nameIn") 
+const inputFunction = document.getElementById("funcionIn")
+const btnGuardar = document.getElementById("btnGuardar")
+// const lista = document.getElementById("lista")
+
+btnGuardar.addEventListener(`click`, () => {
+ console.log(inputNombre.value, inputFunction.value)
+ enciclopedia.push(new Card (inputNombre.value, inputFunction.value, "comodin.jpg"))
+ nuevaCard(enciclopedia)
+ localStorage.setItem("enciclopedia", JSON.stringify(enciclopedia))
+ inputNombre.value = ""
+ inputFunction.value = ""
+
+})
+
+function nuevaCard(array) {
+  let card = document.createElement("div");
+ enciclopedia.forEach ((e) => {
+ card.innerHTML =
+ `<div class="card mb-3" style="max-width: 540px;">
+ <div class="row g-0">
+   <div class="col-md-4">
+     <img src="assets/comodin.jpg" class="img-fluid rounded-start" alt="...">
+   </div>
+   <div class="col-md-8">
+     <div class="card-body">
+       <h5 class="card-title">${e.nombre}</h5>
+       <p class="card-text">${e.fun}</p>
+     </div>
+   </div>
+ </div>
+</div>`} )
+listaCartas.appendChild(card)
+}
+
+// Nueva card storage
 
 
+// Explorar
+const btnExplorar = document.getElementById("btnExplorar")
+const listaCartas= document.getElementById("listaCartas")
 
-      let opcion = parseInt (prompt(`Ingrese el numero correspondiente a la opcion que desea realizar:
-      1 - Explorar la enciclopedia de hechizos
-      2 - Encontrar un hechizo por nombre
-      3- Agregar un nuevo hechizo a la enciclopedia
-      4 - Si desea marcharse`))
+btnExplorar.addEventListener(`click`, () => {
+  explorar(enciclopedia)
+ })
+
+ function explorar(array) {
+  array.forEach((card)=>{
+     let cartas = document.createElement ("div")
+      cartas.innerHTML = 
+      `<div class="card mb-3" style="max-width: 540px;">
+      <div class="row g-0">
+        <div class="col-md-4">
+          <img src="assets/${card.img}" class="img-fluid rounded-start" alt="...">
+        </div>
+        <div class="col-md-8">
+          <div class="card-body">
+            <h5 class="card-title">${card.nombre}</h5>
+            <p class="card-text">${card.fun}</p>
+          </div>
+        </div>
+      </div>
+    </div>` ; 
+      listaCartas.append(cartas);
       
-      switch(opcion){
-          
-          case 1:
-              explorar(enciclopedia)
-            break 
-             case 2: 
-             buscarNombre(enciclopedia)
-            break 
-             case 3: 
-             incluirHechizo(enciclopedia) 
-            break
-          case 4: 
-          salir = true   
-          break
-          
-             default: 
-            alert(`Ingrese una opción correcta`)
-      }
+        
+    })
   }
-   
-  
-  datos()
-  
-  
-  
+//  Ocultar catalogo
+let btnOcultar = document.getElementById ("btnOcultar")
 
+function ocultar () {
+  listaCartas.innerHTML = ""
+}
 
+btnOcultar.addEventListener(`click`, () => {
+  ocultar(enciclopedia)
+ })
 
+// Filtrar
+// let resultado = enciclopedia.filter (el) => el.nombre.includes ("fool")
+ const inputBucar = document.getElementById ("buscar")
+ const btnBuscar = document.getElementById ("btnBuscar")
 
+ function buscar() {
+  let resultado = enciclopedia.filter ((card)=> card.nombre.toLowerCase().includes(inputBucar.value.toLowerCase()))
+  if (resultado == 0){
+    alert ("El nombre ingresado no se encuentra")
+    inputBucar.value = ""
+  } else {
+    listaCartas.innerHTML =""
+    explorar(resultado)
 
+  }
+  }
 
-
-
-
-
+  btnBuscar.addEventListener (`click`, () => {
+  buscar()
+  inputBucar.value = ""
+   })
